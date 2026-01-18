4f3fa877080aefe29797931fdf8aabdec0c0f747
Mark Jharvey <markjharveyc@gmail.com>
2026-01-18 17:00:34 +0800

    fixed models

--- models.py
+++ models.py
@@ -41,6 +41,7 @@ class Withdrawal(db.Model):
     status = db.Column(db.String(20), default="pending")
     requested_at = db.Column(db.DateTime, default=db.func.now())
     processed_at = db.Column(db.DateTime)
+    notify_email = db.Column(db.String(120), nullable=True)
 
 class AdminFund(db.Model):
     id = db.Column(db.Integer, primary_key=True)
